50d2959a9cd49a194bfa98ae03f6c4d9bf12c289
ahmed <ahmed.elghoul@infor.com>
2020-04-26 00:26:35 +0100

    generate gif file

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-## Install
+## Install puppeter without chrome
 
 ```
-PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install
+PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true npm install puppeteer
+```
+
+## Make Gif
+
+To generate Gif from some images PNG:
+```js
+const screens = []
+for (let i = 1; i < 5; ++i) {
+    const filename = path.resolve(__dirname, `IMG${new Date().getTime()}.png`)
+    process.stdout.write('.')
+    screens.push(page.screenshot({ path: filename, fullPage: true }))
+    await sleep(1)
+}
+
+await sleep(1)
+
+await Promise.all(screens)
+
+const encoder = new GIFEncoder(1024, 768)
+
+await pngFileStream(path.resolve(__dirname, 'IMG*png'))
+    .pipe(encoder.createWriteStream({ repeat: 0, delay: 200, quality: 20 }))
+    .pipe(fs.createWriteStream(path.resolve(__dirname, 'screenshot.gif')))
+```
--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const puppeteer = require('puppeteer')
+const path = require('path')
+const fs = require('fs')
+const GIFEncoder = require('gifencoder')
+const pngFileStream = require('png-file-stream')
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 ;(async () => {
     const browser = await puppeteer.launch({
-        headless: false,
+        // headless: false,
         executablePath: '/usr/bin/google-chrome-stable',
     })
     const page = await browser.newPage()
+    page.setViewport({
+        width: 1024,
+        height: 768,
+    })
     await page.goto('http://www.google.com')
+    await page.type('input', 'ahmed ghoul')
 
-    await page.screenshot({ path: 'test.png' })
+    const screens = []
+    for (let i = 1; i < 5; ++i) {
+        const filename = path.resolve(__dirname, `T${new Date().getTime()}.png`)
+        process.stdout.write('.')
+        screens.push(page.screenshot({ path: filename, fullPage: true }))
+        await sleep(1)
+    }
+
+    await sleep(1)
+
+    await Promise.all(screens)
+
+    const encoder = new GIFEncoder(1024, 768)
+
+    await pngFileStream(path.resolve(__dirname, 'T*png'))
+        .pipe(encoder.createWriteStream({ repeat: 0, delay: 200, quality: 20 }))
+        .pipe(fs.createWriteStream(path.resolve(__dirname, 'screenshot.gif')))
 
     await browser.close()
 })()
